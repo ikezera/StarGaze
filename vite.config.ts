82dec137360d73a26f1655aaddf74904ed1da336
ikezera <htmtotal@gmail.com>
2025-02-23 22:22:59 -0300

    json and vite update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/StarGaze/', // Add this line
+  base: '/StarGaze/', // Ensure this matches your repository name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
