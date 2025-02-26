16a3fc5d1b502e516eae905ef931b8738e8b9caf
ikezera <htmtotal@gmail.com>
2025-02-26 12:47:41 -0300

    changing the server js

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // Serve static files from the 'dist' directory
 app.use(express.static(path.join(__dirname, "dist")));
